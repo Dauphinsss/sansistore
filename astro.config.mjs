ad336c640304e43d8becd4137960e18025677943
jh0nz <jhondeycraft776@gmail.com>
2026-05-15 12:37:22 -0400

    chore: add PWA support and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,67 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
+import vitePwa from '@vite-pwa/astro';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
 
-  integrations: [react()],
+  integrations: [
+    react(),
+    vitePwa({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'SansiStore',
+        short_name: 'SansiStore',
+        description: 'Tienda en línea',
+        theme_color: '#88b04b',
+        background_color: '#fffbf4',
+        display: 'fullscreen',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/pwa-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/pwa-1024x1024.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
+  ],
 
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 });
